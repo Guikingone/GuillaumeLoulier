c525bc3637d07fd2b519be0753632becf2deb365
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-07-13 19:38:43 +0200

    [FIX](Core)[!M] Actions|Progress + Routing|YML files added

---
+++ src/AppBundle/Action/HomeAction.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Home project.
+ *
+ * (c) Guillaume Loulier <contact@guillaumeloulier.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Action;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Templating\EngineInterface;
+
+/**
+ * Class HomeAction
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+final class HomeAction
+{
+    /** @var EngineInterface */
+    private $renderEngine;
+
+    /**
+     * HomeAction constructor.
+     *
+     * @param EngineInterface $renderEngine
+     */
+    public function __construct(
+        EngineInterface $renderEngine
+    ) {
+        $this->renderEngine = $renderEngine;
+    }
+
+    /**
+     * @return Response
+     */
+    public function __invoke()
+    {
+        return new Response(
+            $this->renderEngine->render('default/index.html.twig')
+        );
+    }
+}
